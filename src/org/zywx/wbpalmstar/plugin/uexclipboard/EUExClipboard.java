d0b0f34d4c3cc0e67dbc99c9d9b7ca1c12bde3d0
fred <fei.ye@3g2win.com>
2016-06-29 11:29:13 +0800

    uexClipboard 4.0 插件出新

--- src/org/zywx/wbpalmstar/plugin/uexclipboard/EUExClipboard.java
+++ src/org/zywx/wbpalmstar/plugin/uexclipboard/EUExClipboard.java
@@ -1,45 +1,43 @@
 package org.zywx.wbpalmstar.plugin.uexclipboard;
 
-import org.json.JSONException;
-import org.json.JSONObject;
+import android.content.Context;
+import android.text.ClipboardManager;
+
 import org.zywx.wbpalmstar.engine.EBrowserView;
 import org.zywx.wbpalmstar.engine.universalex.EUExBase;
 import org.zywx.wbpalmstar.engine.universalex.EUExCallback;
 
-import android.content.Context;
-import android.text.ClipboardManager;
-import android.util.Log;
-
 public class EUExClipboard extends EUExBase {
 
-	public static final String F_CALLBACK_GET_CONTENT = "uexClipboard.cbGetContent";
-	private ClipboardManager clipboardManager;
+    public static final String F_CALLBACK_GET_CONTENT = "uexClipboard.cbGetContent";
+    private ClipboardManager clipboardManager;
 
-	public EUExClipboard(Context context, EBrowserView inParent) {
-		super(context, inParent);
-		clipboardManager = (ClipboardManager) context.getSystemService(Context.CLIPBOARD_SERVICE);
-	}
+    public EUExClipboard(Context context, EBrowserView inParent) {
+        super(context, inParent);
+        clipboardManager = (ClipboardManager) context.getSystemService(Context.CLIPBOARD_SERVICE);
+    }
 
-	/**
-	 * params[0]--->内容
-	 * 
-	 * @param params
-	 */
-	public void copy(String[] params) {
-		if (params.length > 0) {
-			clipboardManager.setText(params[0]);
-		}
-	}
+    /**
+     * params[0]--->内容
+     *
+     * @param params
+     */
+    public void copy(String[] params) {
+        if (params.length > 0) {
+            clipboardManager.setText(params[0]);
+        }
+    }
 
 
-	public void getContent(String[] params) {
+    public String getContent(String[] params) {
         String value=clipboardManager.getText().toString();
         jsCallback(F_CALLBACK_GET_CONTENT, 0, EUExCallback.F_C_TEXT, value.replace("\n","\\n"));
-	}
+        return value;
+    }
 
-	@Override
-	protected boolean clean() {
-		return true;
-	}
+    @Override
+    protected boolean clean() {
+        return true;
+    }
 
 }
